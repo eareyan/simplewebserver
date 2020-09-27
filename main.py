62f90c2ea6cdf4caf3b183e17fdded6ec8a30715
Enrique <enrique3@gmail.com>
2020-09-27 14:40:26 +0000

    new data route

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import jsonify
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -6,6 +8,11 @@ app = Flask(__name__)
 def hello():
     """ Simple hello worlds """
     return 'Hi!!, Hello World!'
+
+@app.route('/data')
+def show_data():
+    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    return jsonify(df.to_dict())
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
